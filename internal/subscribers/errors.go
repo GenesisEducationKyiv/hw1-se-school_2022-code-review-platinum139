12a9d4339c01a4b5cf165c3fb3b412cd1ce0c652
platinum139 <platinum139.dev@gmail.com>
2022-08-30 17:50:31 +0300

    Refactoring

--- internal/subscribers/errors.go
+++ internal/subscribers/errors.go
@@ -2,11 +2,13 @@ package subscribers
 
 import "strings"
 
+const separator = ","
+
 type SendMailError struct {
 	Subscribers []string
 }
 
 func (e SendMailError) Error() string {
-	emails := strings.Join(e.Subscribers, ",")
+	emails := strings.Join(e.Subscribers, separator)
 	return "Sending email failed for: " + emails
 }
